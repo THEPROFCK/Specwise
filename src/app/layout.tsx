88dbc9e04cf507fa8c2fcf7c423ac0e46b9ea409
Peter Caulcrick <caulcrickp@gmail.com>
2026-02-01 05:13:34 +0100

    Enable Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next";
 // @ts-ignore: import of global CSS without type declarations
 import "./globals.css";
 
